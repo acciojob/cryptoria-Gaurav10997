0961ff8bbe7f1380d82780c847e3958638133ff3
Gaurav Kumar Mandal <96892818+Gaurav10997@users.noreply.github.com>
2023-06-27 06:32:43 +0530

    done with all function script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+
+function decryptMessage(str , shift){
+    let newStr = "";
+    for(let i = 0 ; i< str.length ; i++){
+        newStr+=String.fromCharCode((((((str.charAt(i).charCodeAt(0))+shift)-97)%26)+97))
+    }
+    return newStr;
+}
+
+function sortMessages(arr){
+    const realmsg=[]
+    arr.sort((a,b)=>{
+        return(
+            a.date-b.date
+
+        )
+    })
+    for(let i = 0 ; i< arr.length ; i++){
+         realmsg.push(arr[i].content)
+    }
+    storeMessages(realmsg)
+    return realmsg;
+
+}
+
+
+function storeMessages(arr){
+    localStorage.setItem('messages',arr)
+}
