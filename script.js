763b2bfaccb56a6e15a6656115c9eb03705e3b5d
Gaurav Kumar Mandal <96892818+Gaurav10997@users.noreply.github.com>
2023-06-27 07:46:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,28 +3,29 @@
 function decryptMessage(str , shift){
     let newStr = "";
     for(let i = 0 ; i< str.length ; i++){
+        if(str.charAt(i)==" "){
+            newStr+=" "; 
+            continue;
+        } 
         newStr+=String.fromCharCode((((((str.charAt(i).charCodeAt(0))+shift)-97)%26)+97))
     }
     return newStr;
 }
 
+
 function sortMessages(arr){
     const realmsg=[]
     arr.sort((a,b)=>{
-        return(
-            a.date-b.date
-
-        )
+        return (new Date(a['date']) - new Date(b['date']))    
     })
     for(let i = 0 ; i< arr.length ; i++){
-         realmsg.push(arr[i].content)
+         realmsg.push(arr[i])
     }
-    storeMessages(realmsg)
     return realmsg;
 
 }
 
-
 function storeMessages(arr){
     localStorage.setItem('messages',arr)
 }
+
