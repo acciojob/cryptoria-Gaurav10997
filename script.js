07fb4f491c1c9948c82bb8886cfc31411d40c3a4
Gaurav Kumar Mandal <96892818+Gaurav10997@users.noreply.github.com>
2023-06-27 06:58:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,21 @@
 function decryptMessage(str , shift){
     let newStr = "";
     for(let i = 0 ; i< str.length ; i++){
+        if(str.charAt(i)==" "){
+            newStr+=" "; 
+            continue;
+        } 
         newStr+=String.fromCharCode((((((str.charAt(i).charCodeAt(0))+shift)-97)%26)+97))
     }
     return newStr;
 }
+
+arr1 =  [
+        { date: "2023-04-09", content: "Coduin gchiere" }, 
+        { date: "2023-04-10", content: "Anothe_ test message" }, 
+        { date: "2023-04-11", content: "This is a test message" }, 
+        { date: "2023-04-12", content: "Regisnein ghitke" }
+    ];
 
 function sortMessages(arr){
     const realmsg=[]
@@ -17,14 +28,17 @@ function sortMessages(arr){
         )
     })
     for(let i = 0 ; i< arr.length ; i++){
-         realmsg.push(arr[i].content)
+         realmsg.push(arr[i])
     }
-    storeMessages(realmsg)
     return realmsg;
 
 }
 
 
+
+
 function storeMessages(arr){
     localStorage.setItem('messages',arr)
 }
+
+
